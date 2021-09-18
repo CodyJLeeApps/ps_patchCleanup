0ef258190786a9de5144d4ededb51818be40efea
codyjleeapps <cody.j.lee@icloud.com>
2021-09-18 15:46:51 -0400

    Updating gitignore to include node_modules

---
+++ codecept.conf.js
@@ -0,0 +1,29 @@
+exports.config = {
+  tests: 'tests/*.test.jsx',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'ps_patchCleanup',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
