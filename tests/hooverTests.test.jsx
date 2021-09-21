92ea4b0aaed994616913fc05557701bb4186b0ee
codyjleeapps <cody.j.lee@icloud.com>
2021-09-20 23:00:39 -0400

    Updating mock request body and adding two full workflow tests

--- tests/hooverTests.test.jsx
+++ tests/hooverTests.test.jsx
@@ -1,18 +1,21 @@
 const mockRoomSize = [6, 6];
 const mockOrigin = [0, 0];
 const mockCenter = [3, 3];
-const mockPatches = [];
+const mockEmptyPatches = [];
+const mockStartingLoc = [1, 1];
+const mockPatches = [[1, 4], [2, 5], [3, 3], [4, 3], [5, 1], [5, 5]];
 
 const northDirections = 'NNNNNNN';
 const southDirections = 'SSSSSSS';
 const northEastDirections = 'NENENENE';
 const southWestDirections = 'SWSWSWSW';
+const mockDirections = '';
 
 const mockPostRequestBody = {
     roomSize: mockRoomSize, 
-    coords: mockOrigin, 
-    patches: mockPatches, 
-    instructions: undefined
+    coords: mockStartingLoc, 
+    patches: mockEmptyPatches, 
+    instructions: ''
 };
 
 Feature('Driving Directions and Input Coordinates are properly processed')
@@ -64,4 +67,50 @@ Feature('Driving Directions and Input Coordinates are properly processed')
             `The end location of the hoover was not ${mockOrigin}, it was: ${response.data.coords}`)
     });
 
-// Feature('')
+Feature('Patch cleanup and final Hoover location is properly processed')
+
+    /******************************
+     * Example room for the scenario below
+    _______________________________
+    3 |   |   |   |
+    2 |   | X |   |
+    1 | S |   |   |
+    0 | 1 | 2 | 3 |
+    *******************************/
+    Scenario('when the Hoover moves through one patch of dirt', async ({I}) => {
+        const response = await I.sendPostRequest('/v1/cleaning-sessions', {
+            roomSize: [3, 3],
+            coords: mockStartingLoc,
+            patches: [[2, 2]],
+            instructions: 'EN'
+        });
+        I.assertEqual(response.statusText, 'OK',
+            `The response was not 200 OK, it was ${response.statusText}`);
+        I.assertEqual(response.data.patches, 1);
+        I.assertEqual(response.data.coords, [2, 2]);
+    });
+
+    /******************************
+     * Example room for the scenario below
+     * with `mockPatches` array & `mockStartingLoc`
+    _______________________________
+    6 |   |   |   |   |   |   |
+    5 |   | X |   |   | X |   |
+    4 | X |   |   |   |   |   |
+    3 |   |   | X | X |   |   |
+    2 |   |   |   |   |   |   |
+    1 | S |   |   |   | X |   |
+    0 | 1 | 2 | 3 | 4 | 5 | 6 |
+    *******************************/
+    Scenario('when the Hoover moves through two patches of dirt', async ({I}) => {
+        const response = await I.sendPostRequest('/v1/cleaning-sessions', {
+            roomSize: mockRoomSize,
+            coords: mockStartingLoc,
+            patches: mockPatches,
+            instructions: 'EENNNNEE'
+        });
+        I.assertEqual(response.statusText, 'OK',
+            `The response was not 200 OK, it was ${response.statusText}`);
+        I.assertEqual(response.data.patches, 2);
+        I.assertEqual(response.data.coords, [5, 5]);
+    });
